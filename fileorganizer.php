77e47f163c4a437c2849457a0b03ee4fcddf3f32
carvalho57 <gabrielfeocarvalho57@gmail.com>
2024-02-29 07:34:28 -0300

    feat: adiciona opção type para ordenar por tipo de arquivo

--- fileorganizer.php
+++ fileorganizer.php
@@ -8,19 +8,37 @@ require_once __DIR__ . '/vendor/autoload.php';
 clearstatcache();
 
 $commands = ['type', 'date', 'size'];
-$options = getopt('', ['date', 'type:', 'size', 'source:', 'destination:']);
+$options = getopt('t', ['date', 'type::', 'size', 'source:', 'destination:']);
 
-$source = $options['source'] ?? __DIR__;
-$destination = $options['destination'] ?? __DIR__;
+$source = $options['source'] ?? getcwd();
+$destination = $options['destination'] ?? getcwd();
 
-$source = substr($source, 0, 1) === '/' ? $source : __DIR__ . DIRECTORY_SEPARATOR . $source;
-$destination = substr($destination, 0, 1) === '/' ? $destination : __DIR__ . DIRECTORY_SEPARATOR . $destination;
+$source = substr($source, 0, 1) === '/' ? $source : getcwd() . DIRECTORY_SEPARATOR . $source;
+$destination = substr($destination, 0, 1) === '/' ? $destination : getcwd() . DIRECTORY_SEPARATOR . $destination;
 
 $fileOrganizer = new FileOrganizer();
-$directory = $fileOrganizer->readDirectory($source);
 
 if (array_key_exists('date', $options)) {
+    $directory = $fileOrganizer->readDirectory($source);
     foreach ($directory as $file) {
         FileOrganizer::sortByDate($file, $destination);
+    }
+} elseif (array_key_exists('type', $options)) {
+    $selectedExtensions = $options['type'] !== false ? array_map('trim', explode(',', $options['type'])) : [];
+
+    $directory = $fileOrganizer->readDirectory($source, function (SplFileInfo $file) use ($selectedExtensions) {
+        if (empty($selectedExtensions)) {
+            return true;
+        }
+
+        if (in_array($file->getExtension(), $selectedExtensions)) {
+            return true;
+        }
+
+        return false;
+    });
+
+    foreach ($directory as $file) {
+        FileOrganizer::sortByType($file, $destination);
     }
 }
--- src/FileOrganizer.php
+++ src/FileOrganizer.php
@@ -4,17 +4,16 @@ namespace FileOrganizer;
 
 class FileOrganizer
 {
-    public function readDirectory(string $sourceDirectory, int $level = -1): \RecursiveIteratorIterator
+    public function readDirectory(string $sourceDirectory, callable $filter = null, int $level = -1): \RecursiveIteratorIterator
     {
         $directory = new \RecursiveDirectoryIterator($sourceDirectory, \FilesystemIterator::SKIP_DOTS);
-
-        $files = new \RecursiveCallbackFilterIterator($directory, function (\SplFileInfo $current, $key, $iterator) {
+        $files = new \RecursiveCallbackFilterIterator($directory, function (\SplFileInfo $current, $key, $iterator) use ($filter) {
             if ($iterator->hasChildren()) {
                 return true;
             }
 
             if ($current->isReadable() && !$current->isDir()) {
-                return true;
+                return $filter ? $filter($current) : true;
             }
 
             return false;
@@ -35,6 +34,17 @@ class FileOrganizer
         $year = $creationTime->format('Y');
 
         $destinationPath = $destination . DIRECTORY_SEPARATOR . $year . DIRECTORY_SEPARATOR . $month;
+
+        !is_dir($destinationPath) && mkdir($destinationPath, 0744, true);
+
+        $destinationPath = $destinationPath . DIRECTORY_SEPARATOR . basename($file->getRealPath());
+
+        copy($file->getRealPath(), $destinationPath);
+    }
+
+    public static function sortByType(\SplFileInfo $file, string $destination)
+    {
+        $destinationPath = $destination . DIRECTORY_SEPARATOR . $file->getExtension();
 
         !is_dir($destinationPath) && mkdir($destinationPath, 0744, true);
 
